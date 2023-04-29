07aeddb377ae2d6bdef1c3d426f3e7ef001b28d0
Simon890 <revellosimon890@gmail.com>
2023-04-28 21:04:28 -0300

    Added BFS algo

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -10,18 +10,35 @@ class BreadthFirstSearch:
         """Find path between two points in a grid using Breadth First Search
 
         Args:
-            grid (Grid): Grid of points
-            
-        Returns:
-            Solution: Solution found
-        """
-        # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        grid (Grid): Grid of points
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
+        Returns:
+        Solution: Solution found
+        """
+
+        node = Node("", grid.start, 0)
+        # # Initialize the explored dictionary to be empty
+        explored = {}
         explored[node.state] = True
-        
+
+        if node.state == grid.end: return Solution(node, explored)
+
+        frontier : QueueFrontier = QueueFrontier()
+
+        frontier.add(node)
+        while True:
+            if frontier.is_empty(): return NoSolution(explored)
+            nodo = frontier.remove()
+            neighbours = grid.get_neighbours(nodo.state)
+            explored[nodo.state] = True
+            for position in neighbours:
+                child = Node("", neighbours[position], nodo.cost + grid.get_cost(neighbours[position]), nodo, position)
+                # print(explored)
+                if child.state == grid.end: return Solution(child, explored)
+                if child.state not in explored:
+                    explored[child.state] = True
+                    frontier.add(child)
+
+        # Add the node to the explored dictionary
+
         return NoSolution(explored)
