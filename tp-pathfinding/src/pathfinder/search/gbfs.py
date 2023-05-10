e1047d0d3a128e438129bd7130dab6f2006f2019
Simon890 <revellosimon890@gmail.com>
2023-05-10 10:45:56 -0300

    Added GBFS algo

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -10,18 +10,44 @@ class GreedyBestFirstSearch:
         """Find path between two points in a grid using Greedy Best First Search
 
         Args:
-            grid (Grid): Grid of points
+        grid (Grid): Grid of points
 
         Returns:
-            Solution: Solution found
+        Solution: Solution found
         """
+
+        def h(nodo1 : tuple[int, int]):
+            """
+            Función Heurística
+            Retorna el costo del nodo 1
+            """
+            x1 = nodo1[0]
+            y1 = nodo1[1]
+            x2 = grid.end[0]
+            y2 = grid.end[1]
+            return ((x2 - x1)**2 + (y2 - y1)**2)**(1/2)
+
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        nodo = Node("", grid.start, 0)
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
-        
+        explored = {}
+
         # Add the node to the explored dictionary
-        explored[node.state] = True
-        
+        explored[nodo.state] = nodo
+
+        frontier = PriorityQueueFrontier()
+
+        frontier.add(nodo, 0)
+        while True:
+            if frontier.is_empty(): break
+            nodo = frontier.pop()
+            if nodo.state == grid.end: return Solution(nodo, explored)
+            neighbours = grid.get_neighbours(nodo.state)
+            for position in neighbours:
+                child = Node("", neighbours[position], h(neighbours[position]), nodo, position)
+                if child.state not in explored or child.cost < explored[child.state].cost:
+                    explored[child.state] = child
+                    frontier.add(child, child.cost)
+
         return NoSolution(explored)
