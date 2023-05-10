eaa4ac5daaba3b6531f3580b592e08fbad2c2c73
Simon890 <revellosimon890@gmail.com>
2023-05-10 10:39:59 -0300

    Added DFS algo

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -10,18 +10,33 @@ class DepthFirstSearch:
         """Find path between two points in a grid using Depth First Search
 
         Args:
-            grid (Grid): Grid of points
-            
+        grid (Grid): Grid of points
+
         Returns:
-            Solution: Solution found
+        Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        nodo = Node("", grid.start, 0)
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
-        
+        explored = {}
+
         # Add the node to the explored dictionary
-        explored[node.state] = True
-        
+        explored[nodo.state] = True
+
+        frontier = StackFrontier()
+
+        frontier.add(nodo)
+
+        while True:
+            if frontier.is_empty(): break
+            nodo = frontier.remove()
+            if nodo.state == grid.end: return Solution(nodo, explored)
+            neighbours = grid.get_neighbours(nodo.state)
+            for position in neighbours:
+                child = Node("", neighbours[position], nodo.cost + grid.get_cost(neighbours[position]), nodo, position)
+                if child.state not in explored:
+                    explored[child.state] = True
+                    frontier.add(child)
+
         return NoSolution(explored)
