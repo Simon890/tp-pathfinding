b9a931e468e4b2f52201f5ebc913c6836484cd5d
Simon890 <revellosimon890@gmail.com>
2023-05-10 10:40:54 -0300

    Added UCS algo

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -10,18 +10,35 @@ class UniformCostSearch:
         """Find path between two points in a grid using Uniform Cost Search
 
         Args:
-            grid (Grid): Grid of points
+        grid (Grid): Grid of points
 
         Returns:
-            Solution: Solution found
+        Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        nodo = Node("", grid.start, 0)
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
-        
+        explored = {}
+
         # Add the node to the explored dictionary
-        explored[node.state] = True
-        
+        explored[nodo.state] = nodo
+
+        if nodo.state == grid.end: return Solution(nodo, explored)
+
+        frontier: PriorityQueueFrontier = PriorityQueueFrontier()
+        frontier.add(nodo, 0)
+        while True:
+            if frontier.is_empty(): break
+            nodo = frontier.pop()
+            if nodo.state == grid.end: return Solution(nodo, explored)
+            neighbours = grid.get_neighbours(nodo.state)
+            for position in neighbours:
+                child = Node("", neighbours[position], nodo.cost + grid.get_cost(neighbours[position]), nodo, position)
+                if child.state == grid.end: return Solution(child, explored)
+                if child.state not in explored or child.cost < explored[child.state].cost:
+                    explored[child.state] = child
+                    frontier.add(child, child.cost)
+
+
         return NoSolution(explored)
