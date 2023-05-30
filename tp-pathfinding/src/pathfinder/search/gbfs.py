2f93ec5937b5aaf9366f3fdad024f1ecf34cfa18
Fabio Giampaoli <81629492+Shannon-21@users.noreply.github.com>
2023-05-30 15:38:24 -0300

    Update gbfs.py

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -16,38 +16,47 @@ class GreedyBestFirstSearch:
         Solution: Solution found
         """
 
-        def h(nodo1 : tuple[int, int]):
+        def h(nodo : tuple[int, int]):
             """
-            Función Heurística
-            Retorna el costo del nodo 1
-            """
-            x1 = nodo1[0]
-            y1 = nodo1[1]
-            x2 = grid.end[0]
-            y2 = grid.end[1]
-            return ((x2 - x1)**2 + (y2 - y1)**2)**(1/2)
+            Returns the cost in a straight line between
+            the current node and the objetive node,
+            the euclidian distance.
 
-        # Initialize a node with the initial position
+            Args: 
+                Node.state: tuple(int, int)
+
+            Returns: 
+                float (distance)
+            """
+            x1, y1 = nodo[0], nodo[1]
+            x2, y2 = grid.end[0], grid.end[1]
+
+            return (((x2 - x1)**2) + ((y2 - y1)**2))**(1/2)
+
+        # iniciaiza la busqueda desde la raiz
         nodo = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
         explored = {}
-
-        # Add the node to the explored dictionary
         explored[nodo.state] = nodo
 
         frontier = PriorityQueueFrontier()
+        frontier.add(nodo, h(nodo.state))
 
-        frontier.add(nodo, 0)
         while True:
-            if frontier.is_empty(): break
+            # recorre y elije un nodo de la frontera
+            if frontier.is_empty(): return NoSolution(explored)
             nodo = frontier.pop()
+
             if nodo.state == grid.end: return Solution(nodo, explored)
             neighbours = grid.get_neighbours(nodo.state)
+
+            # expande el nodo seleccionado
             for position in neighbours:
-                child = Node("", neighbours[position], h(neighbours[position]), nodo, position)
+                child = Node("", neighbours[position],
+                             h(neighbours[position]),
+                             nodo, position)
+                
+                # añade el nodo si no ha sido expandido antes, o si su costo es menor al mismo nodo ya expandido
                 if child.state not in explored or child.cost < explored[child.state].cost:
                     explored[child.state] = child
-                    frontier.add(child, child.cost)
-
-        return NoSolution(explored)
+                    frontier.add(child, h(child.state)) # se guarda el nodo con su respectivo valor heuristico
